0f57f68b4747eb64ca85a7e9eda4997763ff4582
Nicholas Ribeiro <90071073+NicholasDRR@users.noreply.github.com>
2023-05-30 18:18:38 -0300

    18:18 30/05/23

---
+++ Arrays/Indexadores/Program.cs
@@ -0,0 +1,38 @@
+﻿Team team = new();
+
+team[0] = "teste";
+team[1] = "teste1";
+team[2] = "teste2";
+
+string test= team[0];
+string test1 = team[1];
+string test2 = team[999];
+
+System.Console.WriteLine(test);
+System.Console.WriteLine(test1);
+System.Console.WriteLine(test2);
+
+public class Team
+{
+    string[] values = new string[10];
+
+    public string this[int i]
+    {
+        get
+        {
+            if (i >= 0 && i < values.Length)
+            {
+                return values[i];
+            }
+            return "Error";
+        }
+        set
+        {
+            if (i >= 0 && i < values.Length)
+            {
+                values[i] = value;
+            }
+        }
+    }
+
+}
---
+++ Arrays/Random/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new(2023);
+
+byte[] teste2 = new byte[10];
+random.NextBytes(teste2);
+
+System.Console.WriteLine("Bytes\n");
+foreach(var item in teste2)
+{
+    System.Console.WriteLine(item);
+}
+
+System.Console.WriteLine("Nexts\n");
+System.Console.WriteLine(random.Next());
+System.Console.WriteLine(random.Next(1));
+System.Console.WriteLine(random.Next(0, 10));
+System.Console.WriteLine(random.NextDouble()\n);
+
+System.Console.WriteLine("Laço For\n");
+
+for (int i = 0; i <=5; i++)
+{
+    System.Console.Write($"{random.Next(1, 10)} ");
+}

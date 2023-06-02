ba8ec5b2c65e835a2c6e3f35ed43d626ba2394ef
Nicholas Ribeiro <90071073+NicholasDRR@users.noreply.github.com>
2023-06-02 20:47:45 -0300

    20:47 02/06/23

---
+++ POO/HerancaNew/Program.cs
@@ -0,0 +1,18 @@
+﻿Student student = new();
+
+student.Name = "Julio";
+student.Course = "Math";
+
+System.Console.WriteLine(student.Salute());
+
+public class Person
+{
+    public string? Name { get; set; }
+    public string Salute() => $"Hi, I'm {Name}";
+}
+public class Student : Person
+{
+    public string? Course { get; set; }
+    public new string Salute()
+         => $"Hi, I'm {Name} from {Course} course";
+}

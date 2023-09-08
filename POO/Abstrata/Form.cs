9d9407d5682ef1baeecbc83812f3bbe1aa4f5082
NicholasDRR <nicholasreis00@gmail.com>
2023-09-08 04:30:52 -0300

    changing gitignore

--- POO/Abstrata/Form.cs
+++ POO/Abstrata/Form.cs
@@ -3,9 +3,9 @@ namespace _Form;
 public abstract class Form 
 {   
 
-    public string Color { get; set; }
+    public string Color { get; set; } 
     public double Area { get; set; }
-    public double Perimeter { get; set; }
+    public double Perimeter { get; set;}
     public abstract double CalculateArea();
     public abstract double CalculatePerimeter();
 
